97aa2f8258bfa05ef754724ba2fa1250da67263a
IgorCCustodio <igorcamposcustodio@gmail.com>
2025-02-11 18:51:43 -0300

    CursoJava 11/02

--- src/main/java/AulaFor/Introducao.java
+++ src/main/java/AulaFor/Introducao.java
@@ -1,4 +1,16 @@
 package AulaFor;
 
 public class Introducao {
+
+    public static void main(String[] args) {
+        for(int contador = 1; contador <=10; contador++){
+
+            //contador++
+            //contador = contador + 1
+            //contador+=1
+
+            System.out.println(contador);
+
+        }
+    }
 }
