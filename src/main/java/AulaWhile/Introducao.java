d438d9f566ba6a97097aa812f6cc8e77ffdbeca1
IgorCCustodio <igorcamposcustodio@gmail.com>
2025-02-18 17:12:15 -0300

    CursoJava 18/02

--- src/main/java/AulaWhile/Introducao.java
+++ src/main/java/AulaWhile/Introducao.java
@@ -2,4 +2,13 @@ package AulaWhile;
 
 public class Introducao {
 
+    public static void main(String[] args) {
+        int contador = 1;
+
+        while(contador <= 10 ){
+
+            System.out.println(contador);
+            contador++;
+        }
+    }
 }
